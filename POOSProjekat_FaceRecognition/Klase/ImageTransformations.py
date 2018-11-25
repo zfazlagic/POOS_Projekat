b16730ac7daf1495f987d5917f324ba3687eeaa2
zerina <zfazlagic1@etf.unsa.ba>
2018-11-25 14:14:13 +0100

    Test klasa

--- POOSProjekat_FaceRecognition/Klase/ImageTransformations.py
+++ POOSProjekat_FaceRecognition/Klase/ImageTransformations.py
@@ -1,5 +1,5 @@
 from PIL import Image
-image_file = Image.open(r"C:\Users\mali_cox\Pictures\test.jpg") # open colour image
+image_file = Image.open("test.png") # open colour image
 image_file = image_file.convert('1') # convert image to black and white
 image_file.save('result.png')
 
